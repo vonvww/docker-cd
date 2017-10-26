639222a4a2c2cf35060e4c448680cd0e1e568de6
Íõά <ww.gggxwy@163.com>
2017-10-26 09:44:18 +0800

    [完美][service-196][master] 提交CD 环境迁移至阿里云相关配置文件

--- ihos/entrypoint.sh
+++ ihos/entrypoint.sh
@@ -1,53 +1,4 @@
 #!/bin/bash
-if [ -d "$WORKDIR" ];
-then
-    cd $WORKDIR
-    git checkout .
-    echo "Pull deploy branch ..."
-    git pull https://${GH_OAUTH_TOKEN}@github.com/propersoft-cn/ihos.git fe-deploy
-fi
-
-if [ ! -d "$WORKDIR" ];
-then
-    echo "Cloning fe-deploy branch ..."
-    git clone https://${GH_OAUTH_TOKEN}@github.com/propersoft-cn/ihos.git -b fe-deploy --depth=1 $WORKDIR
-fi
-
-HC_HOME=/opt/hc
-mkdir -p $HC_HOME
-cd $HC_HOME
-URL=https://cloud.propersoft.cn/teamcities/guestAuth/repository/downloadAll/IHos_Demo/lastSuccessful/artifacts.zip
-echo 'Downloading from '$URL
-curl -O $URL
-if [ -e $HC_HOME/artifacts.zip ];
-then
-    echo 'Unziping artifacts.zip...'
-    rm -rf $HC_HOME/www
-    unzip -qq $HC_HOME/artifacts.zip -d $HC_HOME/www -x index.html *.apk
-    rm -f $HC_HOME/artifacts.zip
-    echo 'Done'
-fi
-
-# app-dev
-if [ -d "$WORKDIR/app-dev" ];
-then
-    cd $WORKDIR/app-dev
-    node proxy/proxy-server.js &
-    cd $WORKDIR/app-dev/www
-    python -m SimpleHTTPServer 9000 &
-    cd $HC_HOME/www
-    python -m SimpleHTTPServer 8000 &
-fi
-
-# app-master
-if [ -d "$WORKDIR/app-master" ];
-then
-    cd $WORKDIR/app-master
-    sed -i "s/8080/8081/" proxy/proxy-server.js
-    node proxy/proxy-server.js &
-    cd $WORKDIR/app-master/www
-    python -m SimpleHTTPServer 9001 &
-fi
 
 # admin
 if [ -d "$WORKDIR/admin" ];
@@ -77,6 +28,17 @@ then
     node proxy/proxy-server.js &
     cd $WORKDIR/app-3.x/www
     python -m SimpleHTTPServer 9004 &
+fi
+
+# app-grey
+if [ -d "$WORKDIR/app-grey" ];
+then
+    cd $WORKDIR/app-grey
+    sed -i "s/8080/8085/" proxy/proxy-server.js
+    node proxy/proxy-server.js &
+    cd $WORKDIR/app-grey/www
+    rm -f index.html
+    python -m SimpleHTTPServer 9005 &
 fi
 
 tail -f /dev/null
---
+++ pea/pea-cd-entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# master
+if [ -d "$WORKDIR/master" ];
+then
+    cd $WORKDIR/master
+    S='"./api"'
+    T='location.protocol+"//"+location.host+"/pea/pep"'
+
+    sed -i "s#$S#$T#g" dist/scripts/scripts*
+    sed -i "s/8080/8081/" proxy/proxy-server.js
+    #node proxy/proxy-server.js &
+    cd $WORKDIR/master/dist
+    python -m SimpleHTTPServer 9001 &
+fi
+
+# merge
+if [ -d "$WORKDIR/merge" ];
+then
+    cd $WORKDIR/merge
+    sed -i "s/8080/8083/" proxy/proxy-server.js
+    node proxy/proxy-server.js &
+    cd $WORKDIR/merge/dist
+    python -m SimpleHTTPServer 9003 &
+fi
+
+# ng2
+if [ -d "$WORKDIR/ng2" ];
+then
+    cd $WORKDIR/ng2
+    S='<base href="/">'
+    T='<base href="/pea/ng2/">'
+
+    sed -i "s#$S#$T#g" dist/index.html
+    sed -i "s/8080/8084/" proxy/proxy-server.js
+    node proxy/proxy-server.js &
+    cd $WORKDIR/ng2/dist
+    python -m SimpleHTTPServer 9004 &
+fi
+
+tail -f /dev/null
